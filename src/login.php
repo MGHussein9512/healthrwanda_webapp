27b0a839d77e9f70090e396b12f182b315602daa
hussein <mugabohussein2019@gmail.com>
2025-11-01 11:31:36 -0700

    Login page is created.

---
+++ src/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>User Login</h2>
+    <form action="login_action.php" method="POST">
+        Email: <input type="email" name="email" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
