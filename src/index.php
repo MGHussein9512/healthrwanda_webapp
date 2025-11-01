deb48033ef45024e8fa3d26cd184eeafdba4b27e
hussein <mugabohussein2019@gmail.com>
2025-11-01 11:07:23 -0700

    Index page is created.

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospital Management System</title>
+</head>
+<body>
+    <h1>Welcome to HEALTHRWANDA Web App</h1>
+    <a href="registration.php">Register</a> |
+    <a href="login.php">Login</a>
+</body>
+</html>
